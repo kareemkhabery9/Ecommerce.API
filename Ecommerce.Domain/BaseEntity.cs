04223b9178ec70398096a86d438b75c3cf273598
kareemkhabery9 <kareemkhabery6@gmail.com>
2025-11-08 13:23:23 +0200

    01 - Product Module [Entities Building]

---
+++ Ecommerce.Domain/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain
+{
+    internal class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; } = default!;
+
+    }
+}
