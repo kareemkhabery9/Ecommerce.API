edbcc3bc823989945fd6118f290e74a4e83fd160
kareemkhabery9 <kareemkhabery6@gmail.com>
2026-02-01 14:13:20 +0200

    Basket Module-Entities

---
+++ Ecommerce.Domain/BasketModule/CustomerBasket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.BasketModule
+{
+
+    // this entity wont be persisted in the database so we dont need to add any EF Core attributes, configurations or Migrations
+    public class CustomerBasket
+    {
+
+        public string Id { get; set; } = default!; // Created From Front-end [GUID]
+
+        ICollection<BasketItem> Items { get; set; } = [];
+    }
+}
