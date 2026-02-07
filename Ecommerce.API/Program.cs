18e9d137df87819f772d6c6bc8aef449ac07e1cf
kareemkhabery9 <kareemkhabery6@gmail.com>
2026-02-07 23:49:23 +0200

    Validation Errors Handler

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -1,6 +1,7 @@
 
 using Ecommerce.API.CustomMiddleware;
 using Ecommerce.API.Externals;
+using Ecommerce.API.Factories;
 using Ecommerce.Domain.Contracts;
 using Ecommerce.Persistence.Data.Data_Seed;
 using Ecommerce.Persistence.Data.DbContexts;
@@ -8,6 +9,7 @@ using Ecommerce.Persistence.Repositories;
 using Ecommerce.Services;
 using Ecommerce.Services.Abstraction;
 using Ecommerce.Services.MappingProfile;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using StackExchange.Redis;
@@ -53,6 +55,11 @@ namespace Ecommerce.API
 
             builder.Services.AddScoped<ICacheRepository,CacheRepository>();
             builder.Services.AddScoped<ICacheService, CacheService>();
+
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = ApiResponseFactory.GenerateApiVaildationResponse;
+            });
 
 
             #endregion
