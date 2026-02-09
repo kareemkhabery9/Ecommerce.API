8bb67d8f6f41cbc4e674eca99f1204d7c7e9f7c9
kareemkhabery9 <kareemkhabery6@gmail.com>
2026-02-09 12:52:41 +0200

    Result Pattern Composition

---
+++ Ecommerce.Shared/CommenResponses/Error.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.CommenResponses
+{
+    public class Error
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public ErrorType ErrorType { get; set; }
+
+        private Error(string code, string discription, ErrorType errorType)
+        {
+            Code=code;
+            Description=discription;
+            ErrorType=errorType;
+        }
+
+        public static Error Failure(string code = "General.Failure", string description = "A General Failure has occured")
+        {
+            return new Error(code, description, ErrorType.Failure);
+        }
+
+
+        public static Error Validation(string code = "General.Validation", string description = "A General Validation has occured")
+        {
+            return new Error(code, description, ErrorType.Validation);
+        }
+
+
+        public static Error NotFound(
+            string code = "Genaral.NotFound",
+            string description = "The Requested Resource was nit found"
+        )
+        {
+            return new Error(code, description, ErrorType.NotFound);
+        }
+
+
+        public static Error UnAuthorized(
+            string code = "Genaral.UnAuthorized",
+            string description = "You are Not Authorized to perform this action"
+        )
+        {
+            return new Error(code, description, ErrorType.UnAuthorized);
+        }
+
+
+        public static Error Forbidden(
+            string code = "Genaral.Forbidden",
+            string description = "You don't have the access to this resource,Access denied"
+        )
+        {
+            return new Error(code, description, ErrorType.Forbidden);
+        }
+
+
+        public static Error InvalidCredintals(
+            string code = "Genaral.InvalidCredintals",
+            string description = "Your Credintals is not valid to reach this resource"
+        )
+        {
+            return new Error(code, description, ErrorType.InvalidCredintals);
+        }
+
+
+    }
+
+}
